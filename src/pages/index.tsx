292a4b832529d5e6268e68b3280bdad9b95374dd
Saeed <s8990@yahoo.com>
2024-12-21 18:18:15 +0330

    feature: add head title in index file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,11 @@
+import Head from "next/head";
+
 export default function Home() {
   return (
     <div>
+        <Head>
+            <title>وبلاگ نوشتار</title>
+        </Head>
       <h1>به وبلاگ نوشتار خوش آمدید</h1>
     </div>
   );
