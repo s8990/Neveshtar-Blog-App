0d9c79fdc3430b975d5ab57cee1424acbcdc3a77
Saeed <s8990@yahoo.com>
2024-12-21 20:33:10 +0330

    refactor: edit default layout header

--- src/components/DefaultLayout/Header/index.tsx
+++ src/components/DefaultLayout/Header/index.tsx
@@ -5,20 +5,20 @@ export default function Header({}) {
         <div className="bg-colorPrimary text-colorPrimaryForeground">
             <div className="container flex flex-col sm:flex-row sm:items-center gap-4">
                 <div className="p-4">
-                    <Link href="#">
+                    <Link href="/">
                         <h6 className="font-semibold text-lg">نوشتار</h6>
                     </Link>
                 </div>
 
                 <ul className="sm:flex-1 flex flex-col sm:flex-row sm:items-center gap-2">
-                    <li className="p-4 hover:bg-colorPrimaryDarker">
-                        <Link href="#">
-                            <p>پست ها</p>
+                    <li>
+                        <Link href="/" className="p-4 hover:bg-colorPrimaryDarker">
+                            پست ها
                         </Link>
                     </li>
-                    <li className="p-4 hover:bg-colorPrimaryDarker">
-                        <Link href="#">
-                            <p>افزودن پست</p>
+                    <li>
+                        <Link href="/add-post" className="p-4 hover:bg-colorPrimaryDarker">
+                            افزودن پست
                         </Link>
                     </li>
                 </ul>
