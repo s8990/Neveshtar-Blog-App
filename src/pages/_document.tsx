0e9ef5f082ce8e395270bb3b9266a5be370eab74
Saeed <s8990@yahoo.com>
2024-12-21 18:11:40 +0330

    fix: remove head title from _document file and move it to index file

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,9 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-        <Head>
-            <title>وبلاگ نوشتار</title>
-        </Head>
+       <Head />
         <body className="antialiased">
         <Main />
         <NextScript />
